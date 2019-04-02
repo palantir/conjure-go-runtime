d250ee9a6c91fdd5008aac77a9895b5a03d98cb6
Peter Larsen <plarsen@palantir.com>
2019-04-02 11:52:36 -0400

    add test

---
+++ conjure-go-client/httpclient/client_test.go
@@ -0,0 +1,39 @@
+package httpclient
+
+import (
+	"bytes"
+	"context"
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestClientImpl_DoRetryWorks(t *testing.T) {
+	count := 0
+	requestBytes := []byte{12, 13}
+
+	server := httptest.NewServer(http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {
+		gotReqBytes, err := ioutil.ReadAll(req.Body)
+		assert.NoError(t, err)
+		assert.Equal(t, requestBytes, gotReqBytes)
+		if count == 0 {
+			rw.WriteHeader(http.StatusBadRequest)
+		}
+		// Otherwise 200 is returned
+		count++
+	}))
+	defer server.Close()
+
+	client, err := NewClient(WithBaseURLs([]string{server.URL}))
+	assert.NoError(t, err)
+
+	_, err = client.Do(
+		context.Background(),
+		WithRawRequestBody(ioutil.NopCloser(bytes.NewReader(requestBytes))),
+		WithRequestMethod(http.MethodPost))
+	assert.NoError(t, err)
+	assert.Equal(t, 2, count)
+}
