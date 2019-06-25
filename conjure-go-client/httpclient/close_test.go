e567a14cf7b50d1dd62785980fa94eb95c09d5ca
Joe Henke <jdhenke@users.noreply.github.com>
2019-06-25 19:06:57 -0400

    [fix] Fix goroutine leak when using error decoder (#52)
    * Fix goroutine leak when using error decoder
    
    * Fix errcheck

--- conjure-go-client/httpclient/close_test.go
+++ conjure-go-client/httpclient/close_test.go
@@ -18,8 +18,10 @@ import (
 	"bytes"
 	"context"
 	"fmt"
+	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
+	"runtime"
 	"runtime/pprof"
 	"testing"
 	"time"
@@ -53,4 +55,98 @@ func TestClose(t *testing.T) {
 	require.NoError(t, pprof.Lookup("goroutine").WriteTo(buf, 1))
 	s := buf.String()
 	assert.NotContains(t, s, "net/http.setRequestCancel")
+}
+
+func TestCloseOnError(t *testing.T) {
+	ts := httptest.NewServer(http.HandlerFunc(func(rw http.ResponseWriter, r *http.Request) {
+		rw.WriteHeader(400)
+		_, _ = fmt.Fprintln(rw, "test")
+	}))
+	defer ts.Close()
+	before := runtime.NumGoroutine()
+	// create test server and client with an HTTP Timeout of 5s
+	client, err := httpclient.NewClient(
+		httpclient.WithBaseURLs([]string{ts.URL}),
+		httpclient.WithHTTPTimeout(5*time.Second),
+	)
+	require.NoError(t, err)
+
+	// execute a simple request
+	ctx := context.Background()
+	_, err = client.Get(
+		ctx,
+		httpclient.WithPath("/"),
+		httpclient.WithHeader("Connection", "close "),
+	)
+	require.Error(t, err)
+
+	// check for bad goroutine before timeout is over
+	time.Sleep(100 * time.Millisecond) // leave some time for the goroutine to reasonably exit
+	buf := bytes.NewBuffer(nil)
+	require.NoError(t, pprof.Lookup("goroutine").WriteTo(buf, 1))
+	s := buf.String()
+	after := runtime.NumGoroutine()
+	assert.Equal(t, before, after, s)
+}
+
+func TestCloseOnEmptyResponse(t *testing.T) {
+	ts := httptest.NewServer(http.HandlerFunc(func(rw http.ResponseWriter, r *http.Request) {
+		rw.WriteHeader(400)
+	}))
+	defer ts.Close()
+	before := runtime.NumGoroutine()
+	// create test server and client with an HTTP Timeout of 5s
+	client, err := httpclient.NewClient(
+		httpclient.WithBaseURLs([]string{ts.URL}),
+		httpclient.WithHTTPTimeout(5*time.Second),
+	)
+	require.NoError(t, err)
+
+	// execute a simple request
+	ctx := context.Background()
+	_, err = client.Get(
+		ctx,
+		httpclient.WithPath("/"),
+		httpclient.WithHeader("Connection", "close "),
+	)
+	require.Error(t, err)
+
+	// check for bad goroutine before timeout is over
+	time.Sleep(1000 * time.Millisecond) // leave some time for the goroutine to reasonably exit
+	buf := bytes.NewBuffer(nil)
+	require.NoError(t, pprof.Lookup("goroutine").WriteTo(buf, 1))
+	s := buf.String()
+	after := runtime.NumGoroutine()
+	assert.Equal(t, before, after, s)
+}
+
+func TestStreamingResponse(t *testing.T) {
+	const (
+		firstLine  = "alpha"
+		secondLine = "bravo"
+	)
+	finishResponseChan := make(chan struct{})
+	ts := httptest.NewServer(http.HandlerFunc(func(rw http.ResponseWriter, r *http.Request) {
+		f, ok := rw.(http.Flusher)
+		require.True(t, ok)
+		_, err := fmt.Fprintln(rw, firstLine)
+		f.Flush()
+		require.NoError(t, err)
+		<-finishResponseChan
+		_, err = fmt.Fprintln(rw, secondLine)
+		require.NoError(t, err)
+		f.Flush()
+	}))
+	defer ts.Close()
+	client, err := httpclient.NewClient(
+		httpclient.WithBaseURLs([]string{ts.URL}),
+	)
+	require.NoError(t, err)
+	ctx := context.Background()
+	resp, err := client.Get(ctx, httpclient.WithPath("/"), httpclient.WithRawResponseBody())
+	require.NoError(t, err)
+	close(finishResponseChan)
+	b, err := ioutil.ReadAll(resp.Body)
+	require.NoError(t, err)
+	assert.Equal(t, firstLine+"\n"+secondLine+"\n", string(b))
 }
