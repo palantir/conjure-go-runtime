0037c5c40825ce6dc9d8fd14f0ae0d682f1c9cf1
Nikita Velat <njvelat@mtu.edu>
2021-02-04 18:17:21 -0500

    timeout refreshable

--- conjure-go-client/httpclient/client_params.go
+++ conjure-go-client/httpclient/client_params.go
@@ -216,7 +216,7 @@ func WithDisableTraceHeaderPropagation() ClientParam {
 // If unset, the client defaults to 1 minute.
 func WithHTTPTimeout(timeout time.Duration) ClientOrHTTPClientParam {
 	return clientOrHTTPClientParamFunc(func(b *httpClientBuilder) error {
-		b.Timeout = timeout
+		b.Timeout = refreshable.NewDuration(refreshable.NewDefaultRefreshable(timeout))
 		return nil
 	})
 }
@@ -472,6 +472,14 @@ func WithRefreshableConfig(config RefreshableClientConfig) ClientParam {
 				return defaultExpectContinueTimeout
 			}
 			return *duration
+		}))
+		b.Timeout = refreshable.NewDuration(config.Map(func(i interface{}) interface{} {
+			// N.B. we only have one timeout field (not based on method) so just take the max of read and write for now.
+			timeout := maxTimeout(i.(ClientConfig).WriteTimeout, i.(ClientConfig).ReadTimeout)
+			if timeout != 0 {
+				return timeout
+			}
+			return defaultClientTimeout
 		}))
 		b.MaxIdleConns = refreshable.NewInt(config.MaxIdleConns().MapIntPtr(func(i *int) interface{} {
 			if i == nil {
