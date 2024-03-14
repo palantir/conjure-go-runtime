ad17165e67bf769ecf8f6ff3a6c5b3cd152ff07e
James Ross <jross@palantir.com>
2024-03-14 15:43:47 -0700

    improvement: expose WithBasicAuthProvider client param.

--- conjure-go-client/httpclient/authn_test.go
+++ conjure-go-client/httpclient/authn_test.go
@@ -80,3 +80,38 @@ func TestRoundTripperWithBasicAuth(t *testing.T) {
 	assert.True(t, wrappedRTInvoked)
 
 }
+
+func TestRoundTripperWithBasicAuthProvider(t *testing.T) {
+	var wrappedRTInvoked bool
+	expected := httpclient.BasicAuth{
+		User:     "user",
+		Password: "password",
+	}
+
+	server := httptest.NewServer(http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {
+		wrappedRTInvoked = true
+		user, pass, ok := req.BasicAuth()
+		assert.True(t, ok)
+		assert.Equal(t, expected.User, user)
+		assert.Equal(t, expected.Password, pass)
+		rw.WriteHeader(http.StatusOK)
+	}))
+	defer server.Close()
+
+	client, err := httpclient.NewClient(
+		httpclient.WithHTTPTimeout(time.Minute),
+		httpclient.WithBasicAuthProvider(func(ctx context.Context) (httpclient.BasicAuth, error) {
+			return httpclient.BasicAuth{
+				User:     "user",
+				Password: "password",
+			}, nil
+		}),
+		httpclient.WithBaseURLs([]string{server.URL}))
+	require.NoError(t, err)
+
+	resp, err := client.Do(context.Background(), httpclient.WithRequestMethod(http.MethodGet))
+	require.NoError(t, err)
+	require.NotNil(t, resp)
+	assert.True(t, wrappedRTInvoked)
+
+}
