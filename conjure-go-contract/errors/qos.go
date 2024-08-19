02502d1a28a6d6f91e8b9ea7882bbd5e17b35022
Brad Moylan <bmoylan@palantir.com>
2024-08-19 12:21:15 -0700

    params

--- conjure-go-contract/errors/qos.go
+++ conjure-go-contract/errors/qos.go
@@ -1,15 +1,32 @@
+// Copyright (c) 2024 Palantir Technologies. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package errors
 
 import (
 	"net/http"
 	"strconv"
 	"time"
+
+	wparams "github.com/palantir/witchcraft-go-params"
 )
 
 type QOSResponse interface {
 	error
 	Status() int
 	Header(http.Header)
+	wparams.ParamStorer
 	isQoS() // marker method
 }
 
@@ -29,6 +46,14 @@ func (q QOSRetryOther) Header(h http.Header) {
 	if q.Location != "" {
 		h.Set("Location", q.Location)
 	}
+}
+
+func (QOSRetryOther) SafeParams() map[string]any {
+	return map[string]any{"statusCode": http.StatusPermanentRedirect}
+}
+
+func (q QOSRetryOther) UnsafeParams() map[string]any {
+	return map[string]any{"location": q.Location}
 }
 
 func (QOSRetryOther) isQoS() {}
@@ -51,6 +76,14 @@ func (q QOSThrottle) Header(h http.Header) {
 	}
 }
 
+func (QOSThrottle) SafeParams() map[string]any {
+	return map[string]any{"statusCode": http.StatusTooManyRequests}
+}
+
+func (QOSThrottle) UnsafeParams() map[string]any {
+	return map[string]any{}
+}
+
 func (QOSThrottle) isQoS() {}
 
 type QOSUnavailable struct{}
@@ -64,5 +97,13 @@ func (QOSUnavailable) Status() int {
 }
 
 func (QOSUnavailable) Header(http.Header) {}
+
+func (QOSUnavailable) SafeParams() map[string]any {
+	return map[string]any{"statusCode": http.StatusServiceUnavailable}
+}
+
+func (QOSUnavailable) UnsafeParams() map[string]any {
+	return map[string]any{}
+}
 
 func (QOSUnavailable) isQoS() {}
