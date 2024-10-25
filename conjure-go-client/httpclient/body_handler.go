0f304fde51c70464020a53e818d876515dac9b2a
Brad Moylan <bmoylan@palantir.com>
2024-10-25 15:06:20 -0700

    demo

--- conjure-go-client/httpclient/body_handler.go
+++ conjure-go-client/httpclient/body_handler.go
@@ -55,47 +55,49 @@ func (b *bodyMiddleware) RoundTrip(req *http.Request, next http.RoundTripper) (*
 
 // setRequestBody returns a function that should be called once the request has been completed.
 func (b *bodyMiddleware) setRequestBody(req *http.Request) (func(), error) {
+	cleanup := func() {} // default no-op cleanup function
+
 	if b.requestInput == nil {
-		return func() {}, nil
+		return cleanup, nil
 	}
 
-	// Special case: if the requestInput is a RequestBody and the requestEncoder is nil,
-	// use the provided input directly as the request body.
-	if b.requestEncoder == nil {
-		requestBody, ok := b.requestInput.(RequestBody)
+	var requestBody RequestBody
+	if b.requestEncoder != nil {
+		if b.bufferPool != nil {
+			// If buffer pool is set, use it with Encode and return a func to return the buffer to the pool.
+			buf := b.bufferPool.Get()
+			cleanup = func() {
+				b.bufferPool.Put(buf)
+			}
+			if err := b.requestEncoder.Encode(buf, b.requestInput); err != nil {
+				cleanup()
+				return nil, werror.WrapWithContextParams(req.Context(), err, "failed to encode request object")
+			}
+			requestBody = RequestBodyInMemory(buf)
+		} else {
+			// If buffer pool is not set, let Marshal allocate memory for the serialized object.
+			buf, err := b.requestEncoder.Marshal(b.requestInput)
+			if err != nil {
+				return nil, werror.WrapWithContextParams(req.Context(), err, "failed to encode request object")
+			}
+			requestBody = RequestBodyInMemory(bytes.NewReader(buf))
+		}
+	} else {
+		// Special case: if the requestInput is a RequestBody and the requestEncoder is nil,
+		// use the provided input directly as the request body.
+		binaryBody, ok := b.requestInput.(RequestBody)
 		if !ok {
 			return nil, werror.ErrorWithContextParams(req.Context(), "requestEncoder is nil but requestInput is not RequestBody",
 				werror.SafeParam("requestInputType", fmt.Sprintf("%T", b.requestInput)))
 		}
-		return func() {}, requestBody.setRequestBody(req)
+		requestBody = binaryBody
 	}
 
-	// If buffer pool is set, use it with Encode and return a func to return the buffer to the pool.
-	if b.bufferPool != nil {
-		buf := b.bufferPool.Get()
-		cleanup := func() {
-			b.bufferPool.Put(buf)
-		}
-		if err := b.requestEncoder.Encode(buf, b.requestInput); err != nil {
-			cleanup()
-			return nil, werror.WrapWithContextParams(req.Context(), err, "failed to encode request object")
-		}
-		if err := RequestBodyInMemory(buf).setRequestBody(req); err != nil {
-			cleanup()
-			return nil, err
-		}
-		return cleanup, nil
-	}
-
-	// If buffer pool is not set, let Marshal allocate memory for the serialized object.
-	buf, err := b.requestEncoder.Marshal(b.requestInput)
-	if err != nil {
-		return nil, werror.WrapWithContextParams(req.Context(), err, "failed to encode request object")
-	}
-	if err := RequestBodyInMemory(bytes.NewReader(buf)).setRequestBody(req); err != nil {
+	if err := requestBody.setRequestBody(req); err != nil {
+		cleanup()
 		return nil, err
 	}
-	return func() {}, nil
+	return cleanup, nil
 }
 
 func (b *bodyMiddleware) readResponse(resp *http.Response, respErr error) error {
