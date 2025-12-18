8bdc8e02c3a5e7203bcda341eee1bc5321f03905
Kevin Simons <ksimons@palantir.com>
2025-12-17 19:05:25 -0800

    Iteration

--- conjure-go-client/httpclient/client_builder.go
+++ conjure-go-client/httpclient/client_builder.go
@@ -109,7 +109,7 @@ func (b *httpClientBuilder) Build(ctx context.Context, params ...HTTPClientParam
 			return toReturn
 		})
 		multiFile := refreshable.NewMultiFileRefreshable(ctx, fileSlices)
-		// This is where we would map
+		// This is where we would map in more CAs
 		tlsParams, _ := refreshable.Merge(b.TransportParams, multiFile, func(t1 refreshingclient.TransportParams, t2 map[string][]byte) refreshingclient.InternalTLSParams {
 			var caBytes [][]byte
 			for _, caSlice := range t2 {
